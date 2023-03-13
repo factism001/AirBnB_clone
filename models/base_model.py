5475e07317acaf7fb080376076b79d3e9c3beec7
chadonet-65 <chadonetthiezar@gmail.com>
2023-03-13 22:30:49 +0000

    update

--- models/base_model.py
+++ models/base_model.py
@@ -22,11 +22,11 @@ class BaseModel:
                         iso = datetime.strptime(
                             iso, '%Y-%m-%dT%H:%M:%S.%f')
                     setattr(self, key, iso)
-         else:
-             self.id = str(uuid.uuid4())
-             self.created_at = datetime.now()
-             self.updated_at = datetime.now()
-             models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            
 
     def __str__(self):
         """

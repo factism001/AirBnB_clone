3d8d7c2636ef13e0db932363d108c798f90cf847
chadonet-65 <107977250+chadonet-65@users.noreply.github.com>
2023-03-13 23:14:40 +0100

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -13,19 +13,20 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Constructor to initialize instance variables"""
-        if kwargs is not None and len(kwargs) != 0:
-            for key, iso in kwargs.items():
-                if key != '__class__':
-                    if (key == 'id'):
-                        self.id = kwargs.get(key)
-                    if key in ['created_at', 'updated_at']:
-                        iso = datetime.strptime(
-                            iso, '%Y-%m-%dT%H:%M:%S.%f')
-                    setattr(self, key, iso)
-                else:
-                    self.id = str(uuid.uuid4())
-                    self.created_at = datetime.now()
-                    self.updated_at = self.created_at
+        if (len(kwargs) is not 0):
+            for key, value in kwargs.items():
+                if (key == 'id'):
+                    self.id = kwargs.get(key)
+                if (key == 'created_at'):
+                    self.created_at = datetime.strptime(
+                        kwargs.get(key), '%Y-%m-%dT%H:%M:%S.%f')
+                if (key == 'updated_at'):
+                    self.updated_at = datetime.strptime(
+                        kwargs.get(key), '%Y-%m-%dT%H:%M:%S.%f')
+         else:
+             self.id = str(uuid.uuid4())
+             self.created_at = datetime.now()
+             self.updated_at = self.created_at
 
     def __str__(self):
         """
